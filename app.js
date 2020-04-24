3aeed9d78fa35991a9047df5cb2cfd20f7f39a5d
Shashank Shekhar <shashank7200@gmail.com>
2020-04-25 00:57:04 +0530

    minor changes =

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
