40ed60eb294aae36cb51cc63385ea8c910af1da4
Shashank Shekhar <shashank7200@gmail.com>
2020-04-25 01:09:47 +0530

    minor update

--- app.js
+++ app.js
@@ -13,6 +13,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.all('/*', (req, res, next) => {
+  
+	res.header('Access-Control-Allow-Origin', req.headers.origin);
+	res.header('Access-Control-Allow-Credentials', true);
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type,token, Access-Control-Request-Method, Access-Control-Request-Headers');
+	if (req.method === 'OPTIONS') {
+	  res.sendStatus(200);
+	} else {
+	  next();
+	}
+});
+
 app.use('/', indexRouter);
 
 module.exports = app;
