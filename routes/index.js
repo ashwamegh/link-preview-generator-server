ef3f127c4c8d6756ea95965cb564f25a685559a8
Shashank Shekhar <shashank7200@gmail.com>
2020-04-24 23:20:47 +0530

    added code for parsing links

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const linkPreviewGenerator = require('link-preview-generator');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/parse/link',async (req, res) => {
+	try {
+		const { url } = req.body;
+		const previewData = await linkPreviewGenerator(url);
+		return res.json(previewData);
+	} catch (error) {
+		return res.status(500).json(error);
+	}
+})
 
 module.exports = router;
