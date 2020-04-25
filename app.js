abc713d6098184ea720e1b2fe76078a1b67eb59b
Shashank Shekhar <shashank7200@gmail.com>
2020-04-25 10:27:37 +0530

    updated app middlewares

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 
@@ -10,21 +12,10 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.all('/*', (req, res, next) => {
-  
-	res.header('Access-Control-Allow-Origin', req.headers.origin);
-	res.header('Access-Control-Allow-Credentials', true);
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-	res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type,token, Access-Control-Request-Method, Access-Control-Request-Headers');
-	if (req.method === 'OPTIONS') {
-	  res.sendStatus(200);
-	} else {
-	  next();
-	}
-});
 
 app.use('/', indexRouter);
 
