a192a26779ead08833af0da5e0a22357f0dd1f46
shashank7200 <shashank7200@gmail.com>
2023-06-22 18:59:25 +0530

    updated source code for railway deployment

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,42 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const linkPreviewGenerator = require('link-preview-generator');
+const linkPreviewGenerator = require("link-preview-generator");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+	res.render("index", { title: "Express" });
 });
 
-router.post('/parse/link',async (req, res) => {
+router.post("/parse/link", async (req, res) => {
 	try {
 		const { url } = req.body;
 		// adds args for puppeteer
 		const previewData = await linkPreviewGenerator(url, [
-			'--no-sandbox',
-			'--disable-setuid-sandbox'
-		  ]);
+			"--no-sandbox",
+			"--disable-setuid-sandbox",
+		]);
 		return res.json(previewData);
 	} catch (error) {
 		return res.status(500).json(error);
 	}
-})
+});
+
+router.post("/health", async (req, res) => {
+	try {
+		const healthcheck = {
+			uptime: `${process.uptime()} seconds`,
+			message: "OK",
+			timestamp: Date.now(),
+		};
+		res.send(healthcheck);
+	} catch (error) {
+		next(
+			createError({
+				status: SERVICE_UNAVAILABLE,
+				message: error,
+			})
+		);
+	}
+});
 
 module.exports = router;
